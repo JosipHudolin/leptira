1ec23356d5b7fe093549b99d05b8c03928d60772
Josip Hudolin <97687545+JosipHudolin@users.noreply.github.com>
2023-06-27 11:41:33 +0200

    Napravljen render Cardova (#3)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+import { UserContext } from "../contexts/UserContext";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../config";
+import BookCard from "../components/BookCard";
 
 const Home = () => {
-  return <Container></Container>;
+  const [books, setBooks] = useState([]);
+
+  const user = useContext(UserContext);
+
+  useEffect(() => {
+    if (!user?.uid) return;
+
+    (async () => {
+      try {
+        const booksRef = collection(db, "book");
+        const q = query(booksRef, where("user", "==", user.uid));
+        const booksSnap = await getDocs(q);
+
+        let tempBooks = [];
+        booksSnap.forEach((doc) =>
+          tempBooks.push({ id: doc.id, ...doc.data() })
+        );
+        setBooks(tempBooks);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [user]);
+
+  return (
+    <Container>
+      {books.map((book) => (
+        <BookCard key={book.id} {...book} />
+      ))}
+    </Container>
+  );
 };
 
 export default Home;
